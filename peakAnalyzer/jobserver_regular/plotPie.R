34fa6ae463153bfa459bd78c8ae24d6cb8ac182d
sokemay@gmail.com <sokemay@gmail.com@577905fa-eddf-4c08-6cf0-cfcde33cab17>
2013-03-22 15:58:05 +0000

    added pie, changed plot size

--- peakAnalyzer/jobserver_regular/plotPie.R
+++ peakAnalyzer/jobserver_regular/plotPie.R
@@ -4,7 +4,7 @@ unmap<-as.numeric(Args[3])
 multimap<-as.numeric(Args[4])
 uniq<-as.numeric(Args[5])
 
-png(paste(outdir,"reads_distribution.png",sep=""))
+png(paste(outdir,"reads_distribution.png",sep=""), width=600)
 slices<-c(unmap,uniq, multimap)
 lab<-c("Non-Mapped", "Unique","Multi-Map")
 pct <- round(slices/sum(slices)*100, digits=2)
