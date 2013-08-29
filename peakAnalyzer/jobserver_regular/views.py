e99760a6d057ee76251196fd8da965e1fc676ee3
yrjie0@gmail.com <yrjie0@gmail.com@577905fa-eddf-4c08-6cf0-cfcde33cab17>
2013-08-29 07:20:12 +0000

    added formatter computing for column charts

--- peakAnalyzer/jobserver_regular/views.py
+++ peakAnalyzer/jobserver_regular/views.py
@@ -107,7 +107,10 @@ def resultfolder_html(dir1):
     for fl in json_file:
         #jsfile=(str(dir1)+"/test1.json").replace(peakAnalyzer.settings.MEDIA_ROOT,"/peakAnalyzer"+peakAnalyzer.settings.MEDIA_URL)
         jsfile=fl.replace(peakAnalyzer.settings.MEDIA_ROOT,"/peakAnalyzer"+peakAnalyzer.settings.MEDIA_URL);
-        html_str+="<script type=\"text/javascript\">\n$(document).ready(function() {$.getJSON(\'"+jsfile+"\', function(data) {var chart = new Highcharts.Chart(data);});})\n</script>\n"
+        html_str+="<script type=\"text/javascript\">\n$(document).ready(function() {$.getJSON(\'"+jsfile+"\', function(data) {"
+        if 'repeat' in dir1 or 'peakAnno' in dir1:
+            html_str+="data.series[0].dataLabels.formatter=eval('('+data.series[0].dataLabels.formatter+')');"
+        html_str+="var chart = new Highcharts.Chart(data);});})\n</script>\n"
         html_str+="<div id=\""+os.path.basename(jsfile).split('.json')[0]+"\" style=\"width: 800px\"></div>\n"
     
     html_str+="</div>\n"
